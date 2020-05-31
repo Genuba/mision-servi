3661a8dec17cb325e31b06a284f73236836a0ca2
faiber.losada <faiber.losada@logytechmobile.com>
2020-05-31 09:52:46 -0500

    se cambia nombre ruta del proyecto

--- app/src/main/java/app/aplicaction/misionservi/PersonaForm.kt
+++ app/src/main/java/app/aplicaction/misionservi/PersonaForm.kt
@@ -41,11 +41,12 @@ class PersonaForm : AppCompatActivity() {
 
             override fun beforeTextChanged(s: CharSequence, start: Int,
                                            count: Int, after: Int) {
+                                    getPersona(s.toString())
             }
 
             override fun onTextChanged(s: CharSequence, start: Int,
                                        before: Int, count: Int) {
-                if(s.toString().length > 6) getPersona(s.toString())
+               // if(s.toString().length > 6) getPersona(s.toString())
             }
         })
     }
